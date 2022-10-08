8327f9794398c6e3b68761dd03634346de3049b2
Bohdan <b.liesnikov+git@gmail.com>
2022-10-08 12:38:31 +0200

    move modules around

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod mlang;
 
+use mlang::expr::*;
+
 fn main() {
-    let v = mlang::mmul(mlang::madd(mlang::mint(1),
-                                    mlang::mint(2)),
-                        mlang::mint(3));
+    let v = mmul(madd(mint(1),
+                      mint(2)),
+                 mint(3));
     let ev = v.eval();
     println!("{}", ev.val());
     println!("Hello, world!")
